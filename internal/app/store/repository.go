1c6fbecf5c0113800f7ee7bfa8a5e72121db93ad
EmmanuelEsperanto <jobamhad@gmail.com>
2025-05-15 09:37:20 +0400

    add middlewares

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -5,5 +5,6 @@ import "educationHTTP-restAPI/internal/app/model"
 // User repo interface ...
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
